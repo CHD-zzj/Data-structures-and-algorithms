645ffe7596ba826314bbe62ca933d0c252539aeb
CHD-zzj <91483882+CHD-zzj@users.noreply.github.com>
2021-11-06 13:38:32 +0800

    Create Eval.c

---
+++ week6/ch04/Eval.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//使用一个表来递归
+double Eval(int n)
+{
+    int i,j;
+    double sum,answer;
+    double *c;
+    c=malloc(sizeof(double)*(n+1));
+    if(c==null)
+        printf("out of space");
+    c[0]=1.0;
+    for(i=1;i<=n;i++)
+    {
+        sum=0.0;
+        for(j=0;j<i;j++)
+            sum+=c[j];
+        c[i]=2.0*sum/i+1;
+    }
+    answer=c[n];
+    free(c);
+    return answer;
+}
